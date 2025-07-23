f36074271368bea31473e99b0b8a62b2069dca28
HanieBaserinik <haniebaserinik@gmail.com>
2025-07-24 03:16:14 +0330

    Update Mainmenuform.cs

--- Mainmenuform.cs
+++ Mainmenuform.cs
@@ -8,56 +8,108 @@ namespace GameCollection
     {
         public MainMenuForm()
         {
-            // تنظیمات فرم
+            InitializeUI();
+            SetupEvents();
+        }
+
+        private void InitializeUI()
+        {
+            // تنظیمات اصلی فرم
             this.Text = "Game Collection";
-            this.Size = new Size(400, 300);
+            this.Size = new Size(500, 400);
             this.StartPosition = FormStartPosition.CenterScreen;
-            
-            // ایجاد دکمه‌ها
-            var btnSnake = new Button {
-                Text = "Snake Game",
-                Size = new Size(200, 50),
-                Location = new Point(100, 30)
+            this.BackColor = Color.FromArgb(240, 240, 240);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Icon = Properties.Resources.GameIcon; // اگر آیکون دارید
+
+            // عنوان برنامه
+            var lblTitle = new Label
+            {
+                Text = "انتخاب بازی",
+                Font = new Font("Tahoma", 18, FontStyle.Bold),
+                ForeColor = Color.DarkBlue,
+                AutoSize = true,
+                Location = new Point(150, 20)
             };
-            
-            var btnCar = new Button {
-                Text = "Car Racing",
-                Size = new Size(200, 50),
-                Location = new Point(100, 100)
+
+            // دکمه بازی مار
+            var btnSnake = new Button
+            {
+                Text = "بازی مار",
+                Size = new Size(250, 60),
+                Location = new Point(125, 80),
+                Font = new Font("Tahoma", 12),
+                BackColor = Color.LightGreen,
+                FlatStyle = FlatStyle.Flat,
+                Cursor = Cursors.Hand
             };
-            
-            var btnGuess = new Button {
-                Text = "Guess Number",
-                Size = new Size(200, 50),
-                Location = new Point(100, 170)
+
+            // دکمه بازی ماشین‌سواری
+            var btnCar = new Button
+            {
+                Text = "مسابقه ماشین",
+                Size = new Size(250, 60),
+                Location = new Point(125, 160),
+                Font = new Font("Tahoma", 12),
+                BackColor = Color.LightSkyBlue,
+                FlatStyle = FlatStyle.Flat,
+                Cursor = Cursors.Hand
             };
-            
-            // رویدادهای کلیک
-            btnSnake.Click += (s, e) => {
-                var snakeForm = new SnakeGameForm();
-                snakeForm.Show();
-                this.Hide();
-                snakeForm.FormClosed += (ss, ee) => this.Show();
+
+            // دکمه بازی حدس عدد
+            var btnGuess = new Button
+            {
+                Text = "حدس عدد",
+                Size = new Size(250, 60),
+                Location = new Point(125, 240),
+                Font = new Font("Tahoma", 12),
+                BackColor = Color.LightSalmon,
+                FlatStyle = FlatStyle.Flat,
+                Cursor = Cursors.Hand
             };
-            
-            btnCar.Click += (s, e) => {
-                var carForm = new CarRacingForm();
-                carForm.Show();
-                this.Hide();
-                carForm.FormClosed += (ss, ee) => this.Show();
-            };
-            
-            btnGuess.Click += (s, e) => {
-                var guessForm = new GuessNumberForm();
-                guessForm.Show();
-                this.Hide();
-                guessForm.FormClosed += (ss, ee) => this.Show();
-            };
-            
-            // اضافه کردن دکمه‌ها به فرم
+
+            // اضافه کردن کنترل‌ها به فرم
+            this.Controls.Add(lblTitle);
             this.Controls.Add(btnSnake);
             this.Controls.Add(btnCar);
             this.Controls.Add(btnGuess);
+        }
+
+        private void SetupEvents()
+        {
+            // پیدا کردن دکمه‌ها
+            var btnSnake = this.Controls.Find("btnSnake", true)[0] as Button;
+            var btnCar = this.Controls.Find("btnCar", true)[0] as Button;
+            var btnGuess = this.Controls.Find("btnGuess", true)[0] as Button;
+
+            // رویدادهای کلیک
+            btnSnake.Click += (s, e) => OpenGameForm(new SnakeGameForm());
+            btnCar.Click += (s, e) => OpenGameForm(new CarRacingForm());
+            btnGuess.Click += (s, e) => OpenGameForm(new GuessNumberForm());
+        }
+
+        private void OpenGameForm(Form gameForm)
+        {
+            this.Hide();
+            gameForm.FormClosed += (s, args) => 
+            {
+                this.Show();
+                this.Activate();
+            };
+            gameForm.Show();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            
+            // نمایش پیام تأیید هنگام بستن برنامه
+            if (MessageBox.Show("آیا می‌خواهید برنامه را ببندید؟", "خروج",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
         }
     }
 }
