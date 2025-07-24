a7ae5df5018b5aa5e96c6139916fad70dc6685d6
HanieBaserinik <haniebaserinik@gmail.com>
2025-07-24 04:00:06 +0330

    Update Settings.cs

--- Settings.cs
+++ Settings.cs
@@ -1,40 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace GameCollection
 {
-    public enum Directions
+    public static class Settings
     {
-        Left, 
-        Right, 
-        Up, 
-        Down
-    };
-
-    class Settings
-    {
-        public static int Width { get; set; }
-        public static int Height { get; set; }
-        public static int Speed { get; set; }
-        public static int Score { get; set; }
-        public static int Points { get; set; }
-        public static bool GameOver { get; set; }
-        public static Directions direction { get; set; }
-
-        public Settings()
-        {
-            Width = 16;
-            Height = 16;
-            Speed = 20;
-            Score = 0;
-            Points = 100;
-            GameOver = false;
-            direction = Directions.Down;
-        }
-
-
+        public static int Width { get; } = 20;
+        public static int Height { get; } = 20;
+        public static int Speed { get; } = 15;
+        public static int Score { get; set; } = 0;
+        public static int Points { get; } = 10;
+        public static bool GameOver { get; set; } = false;
     }
 }
