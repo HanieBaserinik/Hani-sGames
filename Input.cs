e69dbbc9198e952cf1db449e976da3efebd1944b
HanieBaserinik <haniebaserinik@gmail.com>
2025-07-24 03:59:23 +0330

    Update Input.cs

--- Input.cs
+++ Input.cs
@@ -1,39 +1,20 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using System.Collections; // The Hashtable is in the collections class
-using System.Windows.Forms; // The keys are in the Forms Class
+using System.Windows.Forms;
 
 namespace GameCollection
 {
-    class Input
+    public static class Input
     {
-        private static Hashtable keyTable = new Hashtable();
-        // we are creating a new instance of Hashtable class
-        // this class is used to optimize the keys inserted in it
+        private static readonly Dictionary<Keys, bool> KeyTable = new Dictionary<Keys, bool>();
 
         public static bool KeyPress(Keys key)
         {
-            // this function will return a key back to the class
-
-            if (keyTable[key] == null)
-            {
-                // if the hashtable is empty then we return flase
-                return false;
-            }
-            // if the hastable is not empty then we return true
-            return (bool)keyTable[key];
+            return KeyTable.TryGetValue(key, out bool value) && value;
         }
 
-        public static void changeState(Keys key, bool state)
+        public static void ChangeState(Keys key, bool state)
         {
-            // this function will change state of the keys and the player with it
-            // this function has two arguments Key and state
-            keyTable[key] = state;
+            KeyTable[key] = state;
         }
-
     }
 }
